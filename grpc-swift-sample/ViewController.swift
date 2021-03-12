d470d22b0662bb5d6ffe583cf098aed3ee4edd2b
sana <takagisou@users.noreply.github.com>
2021-03-13 03:53:03 +0900

    init

---
+++ grpc-swift-sample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  grpc-swift-sample
+//
+//  Created by sana on 2021/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ grpc-swift-sampleTests/grpc_swift_sampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  grpc_swift_sampleTests.swift
+//  grpc-swift-sampleTests
+//
+//  Created by sana on 2021/03/13.
+//
+
+import XCTest
+@testable import grpc_swift_sample
+
+class grpc_swift_sampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
