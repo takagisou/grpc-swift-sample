e034c4a4f28992591c3a142da812f23f7d6bdb52
sana <takagisou@users.noreply.github.com>
2021-03-13 20:14:53 +0900

    rm sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-protoc ./proto_files/*.proto \
-	--swift_out=./HelloGRPC/Sources/HelloGRPC \
-	--grpc-swift_out=./HelloGRPC/Sources/HelloGRPC \
-	--swift_opt=Visibility=Public \
-	--grpc-swift_opt=Visibility=Public
-
-cd HelloGRPC
-swift build	
